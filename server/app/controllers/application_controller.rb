e1347852bc0aacd37fcf1d5d33a8185b9de192aa
ishibashikenichiro <s1520609@u.tsukuba.ac.jp>
2016-09-07 17:30:47 +0900

    EC2でデプロイするにあたり変更

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+ protect_from_forgery
   def get_string_width(str)
     hankaku_len = str.each_char.count {|x| x.ascii_only? }
     return hankaku_len + (str.size - hankaku_len) * 1.5
